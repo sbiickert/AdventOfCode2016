b00cd1cdd9887fbf3ab2e212a12f23c71847a32a
Simon Biickert <simon@biickert.ca>
2023-07-12 14:51:38 -0600

    Day 3 part 2 complete in Perl

--- Perl/day03.pl
+++ Perl/day03.pl
@@ -32,7 +32,6 @@ exit( 0 );
 
 sub solve_part_one(@input) {
 	my @possible = ();
-	my @impossible = ();
 	for my $line (@input) {
 		$line =~ s/^\s+//; #left trim
 		my @sides = split(/ +/, $line);
@@ -40,14 +39,34 @@ sub solve_part_one(@input) {
 		if ($sides[0] + $sides[1] > $sides[2]) {
 			push(@possible, \@sides);
 		}
-		else {
-			push(@impossible, \@sides);
-		}
 	}
 	
 	say "Part One: the number of possible triangles is " . (scalar @possible);
 }
 
 sub solve_part_two(@input) {
+	my @possible = ();
 
+	my @all = (); 	# first column
+	my @col1 = ();	# second column
+	my @col2 = ();	# third column
+	for my $line (@input) {
+		$line =~ s/^\s+//; #left trim
+		my @nums = split(/ +/, $line);
+		push @all, $nums[0];
+		push @col1, $nums[1];
+		push @col2, $nums[2];
+	}
+	push @all, @col1; # push second column onto first
+	push @all, @col2; # push third column onto the first and second
+	
+	while (scalar @all > 0) {
+		my @sides = (pop(@all), pop(@all), pop(@all)); # take three numbers
+		@sides = sort { $a <=> $b } @sides;
+		if ($sides[0] + $sides[1] > $sides[2]) {
+			push(@possible, \@sides);
+		}
+	}
+	
+	say "Part Two: the number of possible triangles is " . (scalar @possible);
 }
