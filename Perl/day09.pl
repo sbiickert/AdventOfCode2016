9b99620444631609628f7667a07a2004712e7af7
Simon Biickert <simon@biickert.ca>
2023-07-25 16:33:27 -0600

    Day 9 part 2 complete in Perl

--- Perl/day09.pl
+++ Perl/day09.pl
@@ -23,45 +23,75 @@ my $INPUT_PATH = '../input';
 my $INPUT_FILE = 'day09_challenge.txt';
 my @input = read_input("$INPUT_PATH/$INPUT_FILE");
 
-say "Advent of Code 2016, Day 09: Explosives in Cyberspace";
+my $content = $input[0];
 
-solve_part_one($input[0]);
-#solve_part_two(@input);
+say "Advent of Code 2016, Day 09: Explosives in Cyberspace";
+#say $content;
+
+our $re = qr/(\((\d+)x(\d+)\))/;
+
+solve_part_one($content);
+solve_part_two($content);
 
 exit( 0 );
 
 sub solve_part_one($content) {
-	#say $content;
-	my @builder = ();
+	my $result = decompress( $content, 0 );
+	say "Part One: the decompressed length is " . length($result);
+}
+
+sub solve_part_two($content) {
+	my $result = decompress( $content, 1 );
+	say "Part Two: the decompressed length is $result";
+}
+
+# This function returns the decompressed string if $recurse is false.
+# It returns only the LENGTH of the decompressed string if $recurse is true.
+sub decompress( $content, $recurse ) {
+	#say "Decompressing $content";
+	my @builder = ();		# For non-recursive
+	my $result_length = 0;	# For recursive
 	my $index = 0;
-	my $count;
 	
-	while ( $content =~ m/(\((\d+)x(\d+)\))/g) {
+	while ( $content =~ m/$re/g) {
 		my $marker = $1;
 		my $marker_length = length($marker);
 		my $length = $2;
 		my $repeat = $3;
 		my $end_match_index = pos($content);
+		my $prefix_size = $end_match_index - ($index+$marker_length);
 		
-		$count = $end_match_index - ($index+$marker_length);
-		push(@builder, substr($content, $index, $count));
+		my $prefix = substr($content, $index, $prefix_size);
+		my $data = substr($content, $end_match_index, $length);
 		
-		for (1 .. $repeat) {
-			push(@builder, substr($content, $end_match_index, $length));
+		if ($recurse) {
+			$result_length += length($prefix);
+			my $data_length += decompress( $data, 1 );
+			$result_length += $data_length * $repeat;
 		}
+		else {
+			push(@builder, $prefix);
+			for (1 .. $repeat) {
+				push(@builder, $data);
+			}
+		}
+		
+		# Moving pos (the Perl regex pointer for global searches) to the end
+		# of the match (i.e. the marker) plus the length of the data
 		#say "Moving pos from $end_match_index to " . ($end_match_index + $length);
 		$index = $end_match_index + $length;
 		pos($content) = $index;
 	}
 	
-	$count = length($content) - $index;
-	push(@builder, substr($content, $index, $count));
-	
-	my $result = join('', @builder);
-	#say $result;
-	say "Part One: the decompressed length is " . length($result);
-}
-
-sub solve_part_two(@input) {
-
+	my $suffix_size = length($content) - $index;
+	if ($recurse) {
+		$result_length += $suffix_size;
+		return $result_length;
+	}
+	else {
+		push(@builder, substr($content, $index, $suffix_size));
+		my $result = join('', @builder);
+		#say $result;
+		return $result;	
+	}	
 }
